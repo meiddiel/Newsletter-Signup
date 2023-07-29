bc31acb51686ad26ec77502e35df7b1977920f64
Meiddiel <tokymickaelrivo@gmail.com>
2023-07-29 21:36:21 +0300

    add post request using bodyParser

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
 });
   
 app.listen(port, function(){
