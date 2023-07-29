99972a0415b205a4a56f9f09131493998a0d1d36
Meiddiel <tokymickaelrivo@gmail.com>
2023-07-29 20:42:41 +0300

    Adding page copy past from bootstrap and add styleFile

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const request = require('request');
 const app = express();
 const port = 3000;
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/signup.html');
+});
+  
 app.listen(port, function(){
   console.log('Server is running on port ' + port);
 });
